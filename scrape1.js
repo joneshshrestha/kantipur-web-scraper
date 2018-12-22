8b6bc9990679577677621eebe95597b8be2de7cb
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-12-22 16:58:22 +0545

    title, heading, link

---
+++ scrape1.js
@@ -0,0 +1,21 @@
+let request = require('request')
+let cheerio = require('cheerio')
+
+request('https://www.kantipurdaily.com/news', (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        let $ = cheerio.load(html)
+
+        $('.teaser.offset').each((i, el) => {
+            let title = $(el)
+                .find('h2')
+                .text()
+            let link = $(el)
+                .find('a')
+                .attr('href')
+            let heading = $(el)
+                .find('p')
+                .text()
+            console.log(title, link, heading) 
+        })
+    }
+})
