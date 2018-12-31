f2012191ffebcfd8b06a4b8a34844299febd2b8f
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-12-31 21:17:27 +0545

    error loading page

--- scrape1.js
+++ scrape1.js
@@ -1,42 +1,40 @@
-let pug = require ('pug')
-let request = require('request')
-let cheerio = require('cheerio')
-let express = require('express')
-let app = express()
-let array
-   
-app.listen(3000)
+const pug = require ('pug')
+const request = require('request')
+const cheerio = require('cheerio')
+const express = require('express')
 
+const app = express()
 app.set('view engine', 'pug')
 
-request('https://www.kantipurdaily.com/news', (error, response, html) => {
-    if(!error && response.statusCode == 200) {
-        let $ = cheerio.load(html)
+const getNews = (callback) => {
+    request('https://www.kantipurdaily.com/news', (error, response, html) => {
+        if(!error && response.statusCode == 200) {
+            const news = []
+            const $ = cheerio.load(html)
 
-        $('.teaser.offset').each((i, el) => {
-            let title = $(el)
-                .find('h2')
-                .text()
-            let link = $(el)
-                .find('a')
-                .attr('href')
-            let heading = $(el)
-                .find('p')
-                .text()
-            array = [title]
-            console.log(array)
-            console.log(link) 
-            console.log(heading)
-            console.log('\n')
-        })
-    }
-})
+            $('.teaser.offset').each((i, el) => {
+                const title = $(el)
+                    .find('h2')
+                    .text()
+                const link = $(el)
+                    .find('a')
+                    .attr('href')
+                const heading = $(el)
+                    .find('p')
+                    .text()
+                news.push({ title, link, heading })
+            })
+            callback(news)
+        }
+    })
+}
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: array,
-        // link: link,
-        // heading: heading
+    getNews(news => {
+        res.render('index', {
+            title: news 
+        })
     })
 })
 
+app.listen(3000)
