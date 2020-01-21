289cb1d2239a2eb093f65156ca66765bb4596124
joneshshrestha <contact@joneshshrestha.com>
2020-01-21 15:07:28 +0545

    update font

--- scrape1.js
+++ scrape1.js
@@ -29,6 +29,29 @@ const getNews = (callback) => {
     })
 }
 
+// const getNews = (callback) => {
+//     request('https://www.kantipurdaily.com/news', (error, response, html) => {
+//         if(!error && response.statusCode == 200) {
+//             const news = []
+//             const $ = cheerio.load(html)
+
+//             $('.teaser.offset').each((i, el) => {
+//                 const title = $(el)
+//                     .find('h2')
+//                     .text()
+//                 const link = $(el)
+//                     .find('a')
+//                     .attr('href')
+//                 const heading = $(el)
+//                     .find('p')
+//                     .text()
+//                 news.push({ title, link, heading })
+//             })
+//             callback(news)
+//         }
+//     })
+// }
+
 app.get('/', (req, res) => {
     getNews(news => {
         res.render('index', {
